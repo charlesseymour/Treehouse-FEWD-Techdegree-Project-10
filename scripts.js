35521259206d671543f4bb16d7a91e077f5149ae
Charles Seymour <charlesseymour@yahoo.com>
2018-09-25 15:35:34 -0500

    Added AJAX request for user info

--- scripts.js
+++ scripts.js
@@ -1,1 +1,20 @@
- 
+const gridBoxes = document.getElementsByClassName('grid-box-inner');
+
+$.ajax({
+  url: 'https://randomuser.me/api/?results=12',
+  dataType: 'json',
+  success: function(data) {
+    for (let i = 0; i < gridBoxes.length; i++) {
+      let box = gridBoxes[i];
+      let user = data.results[i];
+      box.querySelector('img').src = user.picture.large;
+      box.querySelector('.name').innerHTML = user.name.first + " " + user.  name.last;
+      let email = user.email;
+      let email_name = email.match(/^([^@]*)@/)[1] + "@";
+      box.querySelector('.email_name').innerHTML = email_name;
+      box.querySelector('.email_domain').innerHTML = email.slice(     
+        email_name.length);
+      box.querySelector('.hometown').innerHTML = user.location.city;
+    }
+  }
+});
